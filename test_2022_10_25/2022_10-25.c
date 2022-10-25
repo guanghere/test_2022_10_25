935b01b9a0f3dfe1e0b186b3fcee64a8599aa4d2
HZKD513 <544228362@qq.com>
2022-10-25 14:42:09 +0800

    添加项目文件。

---
+++ test_2022_10_25/2022_10-25.c
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//#include<iomanip>
+//#include<iostream>
+//using::std::cin;
+//using::std::cout;
+//using::std::endl;
+//using::std::setprecision;
+//int compare(int x, int y)
+//{
+//	if (x > y)
+//	{
+//		printf("%d > %d\n", x, y);
+//	}
+//	else if (x == y)
+//	{
+//		printf("%d = %d", x, y);
+//	}
+//	else
+//	{
+//		printf("%d < %d", x, y);
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0, j = 0;
+//
+//	scanf("%d %d", &i, &j);
+//
+//	compare(i,j);
+//
+//	//printf("%d %d", i, j);
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int x = 0, y = 0;
+//	while (scanf("%d %d", &x, &y) != EOF){
+//
+//		if (x > y) {
+//			printf("%d > %d\n", x, y);
+//		}
+//
+//		else if (x == y) {
+//			printf("%d = %d\n", x, y);
+//		}
+//
+//		else{
+//			printf("%d < %d\n", x, y);
+//		}
+//
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	while (n) {
+//		printf("%d", n % 10);
+//		n = n / 10;
+//	}
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	float x = 0, y = 0, z = 0;
+//	cin >> x >> y >> z;
+//	cout << endl;
+//	float j = x + y + z;
+//
+//	printf("%.2f %.2f\n", j, j / 3);
+//	return 0;
+//}
+
+
+
+
+//int max(int x, int y, int z)
+//{
+//	int big = x;
+//	if (y > big)
+//		big = y;
+//	if (z > big)
+//		big = z;
+//
+//	return big;
+//}
+//
+//int main()
+//{
+//
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	float m = 0.0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	max(a, b, c);
+//	m =(float) max(a + b, b, c) / (max(a, b + c, c) + max(a, b, b + c));
+//	printf("%.2f\n", m);
+//
+//	return 0;
+//}
+
+
+
